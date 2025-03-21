9fc24580ffc7a49aa4ba37fe61aca87987664e4f
Siddhartha Bharali <siddharthabharaliassam@gmail.com>
2025-03-21 22:04:12 +0530

    com

--- server/models/Loan.js
+++ server/models/Loan.js
@@ -1,14 +1,38 @@
 const mongoose = require('mongoose');
 
 const LoanSchema = new mongoose.Schema({
-    customer_id: { type: String, required: true },
-    principal: { type: Number, required: true },
-    interest_rate: { type: Number, required: true },
-    tenure: { type: Number, required: true }, // in months
-    total_amount: { type: Number, required: true },
-    emi: { type: Number, required: true },
-    amount_paid: { type: Number, default: 0 },
+    customer_id: { 
+        type: String, 
+        required: true 
+    },
+    principal: { 
+        type: Number, 
+        required: true 
+    },
+    interest_rate: {
+         type: Number, 
+         required: true 
+    },
+    tenure: { 
+        type: Number,
+         required: true 
+    }, 
+    total_amount: {
+         type: Number,
+          required: true
+    },
+    emi: { 
+        type: Number, 
+        required: true 
+    },
+    amount_paid: { 
+        type: Number,
+         default: 0 
+    },
     transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Payment" }]
-}, { timestamps: true });
+}, { 
+    timestamps: true 
+   }
+);
 
 module.exports = mongoose.model('Loan', LoanSchema);
--- server/models/Payment.js
+++ server/models/Payment.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
 const PaymentSchema = new mongoose.Schema({
-    loan_id: { type: mongoose.Schema.Types.ObjectId, ref: "Loan", required: true },
-    amount: { type: Number, required: true },
-    type: { type: String, enum: ["EMI", "LUMP_SUM"], required: true },
-    date: { type: Date, default: Date.now }
+    loan_id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Loan", required: true 
+    },
+    amount: { 
+        type: Number, 
+        required: true 
+    },
+    type: { 
+        type: String, 
+        enum: ["EMI", "LUMP_SUM"], 
+        required: true 
+    },
+    date: { 
+        type: Date, 
+        default: Date.now
+     }
 });
 
 module.exports = mongoose.model('Payment', PaymentSchema);
